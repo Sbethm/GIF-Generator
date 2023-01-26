fe3624c33a471472c6fb7aa8b7ef1eb711f6df02
Sarah <14sbethm@gmail.com>
2023-01-26 10:58:21 -0600

    passed gif state and displayed get results

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Display from './components/Display.jsx';
 
 export default function App() {
   const [ query, setQuery ] = useState('black cat');
-  const [ display, setDisplay ] = useState();
+  const [ gifDisplay, setDisplay ] = useState(null);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -18,8 +18,9 @@ export default function App() {
     if(query.length) {
         fetch(`/gif/${query}`)
         .then((res) => res.json())
-        .then((gif) => {
-          console.log('Here is your result!', gif);
+        .then((array) => {
+          console.log('Here is your result!', array);
+          setDisplay(array)
         })
         .catch(err => { 
             console.log("YOUR POST WAS NOT SUCCESSFUL", err);
@@ -34,7 +35,7 @@ export default function App() {
     <>
       <h1>GIF Generator</h1>
       <Input onSubmit={ onSubmit } />
-      <Display />
+      <Display gifs={ gifDisplay } />
     </>
   )
 }
