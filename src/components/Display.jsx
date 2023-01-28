57f8c4e8cc2979f38c820173f37b41332e82b3c7
Sarah <14sbethm@gmail.com>
2023-01-28 14:37:23 -0600

    styled copy btn to gif top right corner

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,35 +1,17 @@
 import React, { useState } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Gif from './Gif.jsx';
 
 export default function Display(props) {
     const gifDisplay = props.gifs;
-    const [ isCopied, setIsCopied ] = useState(false);
-
-    const copied = () => {
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 1000);
-    }
 
     const gifElements = gifDisplay && gifDisplay.map((gif, i) => {
         return (
-            <div className='gif--container flex-center'>
-                <video 
-                    // width="325" 
-                    key={`gif--video${i}`} 
-                    controls 
-                    autoPlay 
-                    loop 
-                    muted>
-                    <source 
-                        src={`${ gif.mp4 }`} 
-                        type="video/mp4" 
-                        alt={`${ gif.title }`} 
-                        key={`gif--source${i}`} />
-                </video>
-                <CopyToClipboard className='copyBtn' text={ gif.mp4 } onCopy={ () => copied() }>
-                  <button key={`copy--btn${i}`}>{ isCopied ? 'Copied!' : 'Copy' }</button>
-                </CopyToClipboard>
-            </div>
+           <Gif 
+              gif={ gif } 
+            //   copied={ copied } 
+            //   isCopied={ isCopied } 
+              key={`gif--video${i}`} 
+            />
         )
     });
 
