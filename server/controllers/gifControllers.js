9aab85e9d73113ff30812a76567d9bbdc7bfc43e
Sarah <14sbethm@gmail.com>
2023-01-28 20:02:58 -0600

    completed the backend and frontend handling of three random gifs on load

--- server/controllers/gifControllers.js
+++ server/controllers/gifControllers.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const gifControllers = {
     
-    getGIF: (req, res, next) => {
+    getQueriedGif: (req, res, next) => {
         //convert params to fit url fetch request specs
         const query = req.params.query.replaceAll(' ', '+');
 
@@ -12,15 +12,47 @@ const gifControllers = {
             const data = res.data.data;
             //reduce response to object with mp4 and title key value pairs 
             const resArr = data.map((obj) => {
-                const { title } = obj;
                 const { mp4 } = obj.images.original;
-                return {title, mp4 };
+                return { mp4 };
             })
             return resArr;
         })
         .then((array) => {
             //store response as an array of objects (25)
             res.locals.array = array;
+            return next();
+        })
+    },
+
+    getRandomGif: (req, res, next) => {
+
+        axios
+        .get(`https://api.giphy.com/v1/gifs/random?api_key=pjU0gj8bjFNq3pwK1DFC8Mk08GXlJ7Lz&tag=&rating=g`)
+        .then((res) => {
+            const data = res.data.data;
+            const { mp4 } = data.images.original;
+            const resArr = [{ mp4 }];
+            return resArr;
+        })
+        .then((array) => {
+            //store response as an array of objects
+            res.locals.array = array;
+            return next();
+        })
+    },
+
+    addRandomGif: (req, res, next) => {
+
+        axios
+        .get(`https://api.giphy.com/v1/gifs/random?api_key=pjU0gj8bjFNq3pwK1DFC8Mk08GXlJ7Lz&tag=&rating=g`)
+        .then((res) => {
+            const data = res.data.data;
+            const { mp4 } = data.images.original;
+            return { mp4 };
+        })
+        .then((array) => {
+            //store response as an array of objects
+            res.locals.array.push(array);
             return next();
         })
     }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Input from './components/Input';
 import Display from './components/Display';
 
 export default function App() {
   const [ query, setQuery ] = useState('');
   const [ gifDisplay, setGifDisplay ] = useState(null);
+
+  //fetch 3 random gifs for onload
+  useEffect(() => {
+    fetch(`/gif`)
+      .then((res) => res.json())
+      .then((array) => {
+        setGifDisplay(array);
+      })
+      .catch(err => { 
+          console.log("YOUR REQUEST WAS NOT SUCCESSFUL", err);
+      })
+  }, [])
 
   const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
     const { value } = event.target as HTMLInputElement;
@@ -18,15 +30,11 @@ export default function App() {
         fetch(`/gif/${query}`)
         .then((res) => res.json())
         .then((array) => {
-          if(gifDisplay) {
-            setGifDisplay(null)
-            setGifDisplay(array)
-          } else {
-            setGifDisplay(array)
-          }
+          setGifDisplay(null)
+          setGifDisplay(array)
         })
         .catch(err => { 
-            console.log("YOUR POST WAS NOT SUCCESSFUL", err);
+            console.log("YOUR REQUEST WAS NOT SUCCESSFUL", err);
         })
     } else {
         window.alert("Please fill in every box.")
@@ -39,7 +47,7 @@ export default function App() {
     <div className='main--container'>
       <h1>GIF Generator</h1>
       <Input onSubmit={ onSubmit } handleChange={ handleChange } />
-      <Display gifs={ gifDisplay } />
+      <Display gifDisplay={ gifDisplay } />
     </div>
   )
 }
