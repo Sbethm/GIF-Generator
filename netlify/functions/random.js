5433c6bda44ae4e52427f8ff35fdf596613ccf8c
Sarah <14sbethm@gmail.com>
2023-01-30 10:49:33 -0600

    cleaning net function code

--- netlify/functions/random.js
+++ netlify/functions/random.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
-  console.log(event)
+
   try {
 
     return Promise.all([
@@ -18,26 +18,11 @@ const handler = async (event) => {
         return resArr;
       })
     .then(array => {
-        console.log("THIS IS RESOPNSE", array)
         return { 
             statusCode: 200,
             body: JSON.stringify({ array: array }),
         };
     })
-
-    // return fetch('https://api.giphy.com/v1/gifs/random?api_key=pjU0gj8bjFNq3pwK1DFC8Mk08GXlJ7Lz&tag=&rating=g')
-    //     .then(res => res.json())
-    //     .then(res =>  res.data)
-    //     .then(data => {
-    //         console.log("THIS IS RESOPNSE", data)
-    //         const { mp4 } = data.images.original;
-    //         console.log("THIS IS OBJ", mp4)
-    //         return { 
-    //             statusCode: 200,
-    //             body: JSON.stringify({ array: [ { mp4 } ] }),
-    //         };
-    //     })
-
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
